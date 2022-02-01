791ce6578471e054dd8430c53c12f7a2967027c9
Damilola Michael Ige <idm.calculus@gmail.com>
2022-02-01 16:25:49 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by IGE DAMILOLA MICHAEL on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
